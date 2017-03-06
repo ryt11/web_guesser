0ef4e00f0c6d1445a58cf3c78551785342fdb763
ryt11 <rileyt_2000@hotmail.com>
2017-03-05 17:09:11 -0700

    add cheat extension

--- guess_checker.rb
+++ guess_checker.rb
@@ -3,10 +3,11 @@ require_relative 'message_master'
 class GuessChecker
 
   attr_reader :num, :guess, :correct
-  attr_accessor :correct
-  def initialize(num, guess)
+  attr_accessor :correct, :cheat
+  def initialize(num, guess, cheat)
     @num = num
     @guess = guess.to_i
+    @cheat = cheat
     @correct = false
   end
 
@@ -35,6 +36,20 @@ class GuessChecker
     (num - guess) > 5
   end
 
+  def cheat?
+    cheat == 'true'
+  end
+
+def check_guess (guess, cheat)
+  if params['guess'].nil?
+      message = "Make a guess!"
+      erb :index, :locals => {:message => message, :correct => false, :cheat => false}
+  else
+    gc = GuessChecker.new(number, params['guess'], params['cheat'])
+    message = gc.which_output
+    erb :index, :locals => {:message => message, :correct => gc.correct, :show_number => gc.show_number, :cheat => gc.cheat?}
+  end
+end
   def which_output
     mm = MessageMaster.new
     if correct?
--- web_guesser.rb
+++ web_guesser.rb
@@ -6,14 +6,13 @@ require 'pry'
 
 number = rand(100)
 mm = MessageMaster.new
-
 get '/' do
   if params['guess'].nil?
       message = "Make a guess!"
-      erb :index, :locals => {:message => message, :correct => false}
+      erb :index, :locals => {:message => message, :correct => false, :cheat => false}
   else
-    gc = GuessChecker.new(number, params['guess'])
+    gc = GuessChecker.new(number, params['guess'], params['cheat'])
     message = gc.which_output
-    erb :index, :locals => {:message => message, :correct => gc.correct, :show_number => gc.show_number}
+    erb :index, :locals => {:message => message, :correct => gc.correct, :show_number => gc.show_number, :cheat => gc.cheat?}
   end
 end
